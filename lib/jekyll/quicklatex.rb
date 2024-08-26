e732c131a79ab6f59e7f9dcb6581f98242567524
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2024-08-26 16:55:11 +0800

    Update quicklatex.rb

--- lib/jekyll/quicklatex.rb
+++ lib/jekyll/quicklatex.rb
@@ -6,89 +6,50 @@ require 'jekyll/quicklatex/version'
 module Jekyll
   module Quicklatex
     class Block < Liquid::Block
-      def initialize tag_name, text, tokens
-        super
-        @text = text
-      end
-    
-    def render(context)
-      "{% raw %}#{@text}{% endraw %}"
+    Syntax = /\A\s*\z/
+
+    def initialize(tag_name, markup, parse_context)
+      super
+
+      ensure_valid_markup(tag_name, markup, parse_context)
     end
 
-      def init_param
-        @site_uri = URI('https://quicklatex.com/latex3.f')
-        @post_param = {
-          :fsize => '30px',
-          :fcolor => '000000',
-          :mode => 0,
-          :out => 1,
-          :errors => 1,
-          :remhost => 'quicklatex.com',
-        }
-        @pic_regex = /https:\/\/quicklatex.com\/cache3\/[^\.]*/
-        @saved_dir = 'assets/latex'
+    def parse(tokens)
+      @body = +''
+      while (token = tokens.shift)
+        if token =~ BlockBody::FullTokenPossiblyInvalid && block_delimiter == Regexp.last_match(2)
+          parse_context.trim_whitespace = (token[-3] == WhitespaceControl)
+          @body << Regexp.last_match(1) if Regexp.last_match(1) != ""
+          return
+        end
+        @body << token unless token.empty?
       end
 
-      def filter_snippet(snippet)
-        # text is html
-        # strip all html tags
-        no_html_tag = snippet.gsub(/<\/?[^>]*>/, "")
-          .gsub(/&gt;/, '>')
-          .gsub(/&lt;/, '<')
+      raise_tag_never_closed(block_name)
+    end
 
-        # strip all comments in latex code snippet
-        lines = no_html_tag.lines
-        lines.reject do |l|
-          # blank line or comments(start with %)
-          l =~ /^\s*$/ or l =~ /^\s*%/
-        end.join
-      end
+    def render_to_output_buffer(_context, output)
+      output << @body
+      output
+    end
 
-      def seperate_snippet(snippet)
-        lines = snippet.lines
-        preamble, formula = lines.partition { |line| line =~ /usepackage/ }
+    def nodelist
+      [@body]
+    end
 
-        def join_back(lines)
-          lines.join('').gsub(/%/, '%25').gsub(/&/, '%26')
-        end
+    def blank?
+      @body.empty?
+    end
 
-        return {
-          :preamble => join_back(preamble),
-          :formula => join_back(formula),
-        }
-      end
+    protected
 
-      def remote_compile(snippet)
-
-        param = @post_param.merge(seperate_snippet(snippet))
-
-        req = Net::HTTP::Post.new(@site_uri)
-        body_raw = param.inject('') do |result, nxt|
-          "#{result}&#{nxt[0].to_s}=#{nxt[1].to_s}"
-        end
-        req.body = body_raw.sub('&', '')
-
-        res = Net::HTTP.start(@site_uri.hostname, @site_uri.port, use_ssl: true) do |http|
-          http.request(req)
-        end
-
-        case res
-        when Net::HTTPSuccess, Net::HTTPRedirection
-          puts res.body
-          pic_uri = URI(res.body[@pic_regex]+'.svg')
-          puts pic_uri
-
-          Net::HTTP.start(pic_uri.host, use_ssl: true) do |http|
-            # http get
-            resp = http.get(pic_uri.path)
-            return resp.body
-          end
-        else
-          res.value
-        end
+    def ensure_valid_markup(tag_name, markup, parse_context)
+      unless Syntax.match?(markup)
+        raise SyntaxError, parse_context.locale.t("errors.syntax.tag_unexpected_args", tag: tag_name)
       end
     end
   end
 end
+
 
 Liquid::Template.register_tag('latex', Jekyll::Quicklatex::Block)
