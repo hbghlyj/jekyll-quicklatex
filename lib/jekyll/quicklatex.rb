9f541c4ca2207b30d9fce3182b0c9b67d2be888a
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2024-08-26 16:24:42 +0800

    Update quicklatex.rb

--- lib/jekyll/quicklatex.rb
+++ lib/jekyll/quicklatex.rb
@@ -22,7 +22,7 @@ module Jekyll
           @body << token unless token.empty?
         end
   
-        raise_tag_never_closed(block_name)
+        Liquid::Block::raise_tag_never_closed(block_name)
       end
     
       def render(context)
