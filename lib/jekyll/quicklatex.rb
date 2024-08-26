7d64395936891dfc8be58a061aacc573f5392d95
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2024-08-26 18:15:48 +0800

    Update quicklatex.rb

--- lib/jekyll/quicklatex.rb
+++ lib/jekyll/quicklatex.rb
@@ -1,169 +1,59 @@
-require 'net/http'
-require 'fileutils'
-require 'digest'
-require 'jekyll/quicklatex/version'
+# frozen_string_literal: true
 
-module Jekyll
-  module Quicklatex
-    class Block < Liquid::Block
-      Syntax = /\A\s*\z/
+module Liquid
+  # @liquid_public_docs
+  # @liquid_type tag
+  # @liquid_category syntax
+  # @liquid_name raw
+  # @liquid_summary
+  #   Outputs any Liquid code as text instead of rendering it.
+  # @liquid_syntax
+  #   {% raw %}
+  #     expression
+  #   {% endraw %}
+  # @liquid_syntax_keyword expression The expression to be output without being rendered.
+  class Raw < Block
+    Syntax = /\A\s*\z/
 
-      def initialize(tag_name, markup, parse_context)
-        super
-        init_param
-        ensure_valid_markup(tag_name, markup, parse_context)
-      end
-      #Start raw: https://github.com/Shopify/liquid/blob/main/lib/liquid/tags/raw.rb
-      def parse(tokens)
-        @body = +''
-        while (token = tokens.shift)
-          if token =~ /\A(.*)\{\%-?\s*(\w+)\s*(.*)?-?\%\}\z/om && block_delimiter == Regexp.last_match(2)
-            parse_context.trim_whitespace = (token[-3] == '-')
-            @body << Regexp.last_match(1) if Regexp.last_match(1) != ""
-            return
-          end
-          @body << token unless token.empty?
+    def initialize(tag_name, markup, parse_context)
+      super
+
+      ensure_valid_markup(tag_name, markup, parse_context)
+    end
+
+    def parse(tokens)
+      @body = +''
+      while (token = tokens.shift)
+        if token =~ BlockBody::FullTokenPossiblyInvalid && block_delimiter == Regexp.last_match(2)
+          parse_context.trim_whitespace = (token[-3] == WhitespaceControl)
+          @body << Regexp.last_match(1) if Regexp.last_match(1) != ""
+          return
         end
-      end
-
-      def render(context)
-        @output_dir = context.registers[:site].config['destination']
-        "<img src='/#{remote_compile @body}'/>"
-      end
-
-      def nodelist
-        [@body]
+        @body << token unless token.empty?
       end
 
-      def blank?
-        @body.empty?
-      end
+      raise_tag_never_closed(block_name)
+    end
 
-      protected
+    def render_to_output_buffer(_context, output)
+      output << @body
+      output
+    end
 
-      def ensure_valid_markup(tag_name, markup, parse_context)
-        unless Syntax.match?(markup)
-          raise SyntaxError, parse_context.locale.t("errors.syntax.tag_unexpected_args", tag: tag_name)
-        end
-      end
-      #End raw
+    def nodelist
+      [@body]
+    end
 
-      #Start QuickLatex: https://github.com/DreamAndDead/jekyll-quicklatex/blob/master/lib/jekyll/quicklatex.rb
-      class Cache
-        def initialize
-          @cache = {}
-          @cache_file = 'latex.cache'
-          if File.exist? @cache_file
-            File.open(@cache_file, 'r') do |f|
-              while line = f.gets
-                hash, url = line.split
-                @cache[hash] = url
-              end
-            end
-          end
-        end
+    def blank?
+      @body.empty?
+    end
 
-        def fetch(content)
-          id = hash_id(content)
-          @cache[id]
-        end
+    protected
 
-        def cache(content, url)
-          id = hash_id(content)
-          @cache[id] = url
-          File.open(@cache_file, 'a') do |f|
-            f.syswrite("#{id} #{url}\n")
-          end
-        end
-
-        private
-
-        def hash_id(content)
-          Digest::MD5.hexdigest(content)
-        end
-      end
-      
-      def init_param
-        @site_uri = URI('https://quicklatex.com/latex3.f')
-        @post_param = {
-          :fsize => '30px',
-          :fcolor => '000000',
-          :mode => 0,
-          :out => 1,
-          :errors => 1,#report LaTeX errors
-          :remhost => 'quicklatex.com',
-        }
-        @pic_regex = /https:\/\/quicklatex.com\/cache3\/[^\.]*/
-        @saved_dir = 'assets/latex'
-        @cache = Cache.new
-      end
-
-      def seperate_snippet(snippet)
-        lines = snippet.lines
-        preamble, formula = lines.partition { |line| line =~ /usepackage/ }
-
-        def join_back(lines)
-          lines.join('').gsub(/%/, '%25').gsub(/&/, '%26')#QuickLatex-customized form encoding of &
-        end
-
-        return {
-          :preamble => join_back(preamble),
-          :formula => join_back(formula),
-        }
-      end
-
-      def remote_compile(snippet)
-        if url = @cache.fetch(snippet)
-          return url
-        end
-        
-        param = @post_param.merge(seperate_snippet(snippet))
-
-        req = Net::HTTP::Post.new(@site_uri)
-        body_raw = param.inject('') do |result, nxt|
-          "#{result}&#{nxt[0].to_s}=#{nxt[1].to_s}"
-        end
-        req.body = body_raw.sub('&', '')
-
-        res = Net::HTTP.start(@site_uri.hostname, @site_uri.port, use_ssl: true) do |http|
-          http.request(req)
-        end
-
-        case res
-        when Net::HTTPSuccess, Net::HTTPRedirection
-          puts res.body
-
-          #assert_equal the first char of the response is the char "0"
-          if res.body[0] != '0'
-            raise "QuickLatex Error: #{res.body}"
-          end
-
-          pic_uri = URI(res.body[@pic_regex]+'.svg')
-          puts pic_uri
-          
-          save_path = "#{@saved_dir}#{pic_uri.path}"
-          dir = File.dirname(save_path)
-          unless File.directory? dir
-            FileUtils.mkdir_p dir
-          end
-
-          @cache.cache(snippet, pic_uri.path)
-          
-          Net::HTTP.start(pic_uri.host, use_ssl: true) do |http|
-            # https get
-            resp = http.get(pic_uri.path)
-            File.open(save_path, "w") do |file|
-              file.write(resp.body)
-            end
-          end
-          save_path
-        else
-          res.value
-        end
+    def ensure_valid_markup(tag_name, markup, parse_context)
+      unless Syntax.match?(markup)
+        raise SyntaxError, parse_context.locale.t("errors.syntax.tag_unexpected_args", tag: tag_name)
       end
     end
   end
 end
-
-
-Liquid::Template.register_tag('latex', Jekyll::Quicklatex::Block)
